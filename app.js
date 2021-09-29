d860446d00907f2901bc0fab9ba05ce14dd2efd3
Prashil Ramteke <87717960+prashil03081999@users.noreply.github.com>
2021-09-29 12:58:39 +0530

    tut72

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require("express");
+const path =require('path')
 const app = express();
 const port = 80;
+
+// For serving static files
+app.use('/static', express.static('static'))
+
+// Set the template engine as pug
+app.set('view engine', 'pug')
+
+// Set the views directory
+app.set('views', path.join(__dirname, 'views'))
+
+// Our pug demo endpoint
+app.get("/demo", (req, res)=>{ 
+    res.status(200).render('demo', { title: 'Hey Pr', message: 'Hello there and thanks for telling me how to use pubG!' })
+});
 
 app.get("/", (req, res)=>{ 
     res.send("This is homepage of my first express app with Prashil");
