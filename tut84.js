93d3c198ec54a23181abd2213b6f19150b0a3834
Prashil Ramteke <87717960+prashil03081999@users.noreply.github.com>
2021-10-01 21:04:24 +0530

    Updating data from the Mongo Databas

---
+++ tut84.js
@@ -0,0 +1,9 @@
+// show dbs 
+// use harryKart
+// show collections
+
+
+db.items.find()
+db.items.updateOne({name: "Moto 30s"}, {$set: {price: 2}})
+db.items.find()
+db.items.updateMany({name: "Moto 30s"}, {$set: {price: 3, rating: 1}})
