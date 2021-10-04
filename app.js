62096616559a595305002ab56f905dbc9256d744
Prashil Ramteke <87717960+prashil03081999@users.noreply.github.com>
2021-10-04 23:16:11 +0530

    tut73

--- app.js
+++ app.js
@@ -1,35 +1,43 @@
+
 const express = require("express");
-const path =require('path')
 const app = express();
+const path = require("path");
+const fs = require("fs");
 const port = 80;
 
-// For serving static files
-app.use('/static', express.static('static'))
+// EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static')) // For serving static files
+app.use(express.urlencoded());
 
-// Set the template engine as pug
-app.set('view engine', 'pug')
+// PUG SPECIFIC STUFF
+app.set('view engine', 'pug') // Set the template engine as pug
+app.set('views', path.join(__dirname, 'views')) // Set the views directory
 
-// Set the views directory
-app.set('views', path.join(__dirname, 'views'))
+// ENDPOINTS
+    app.get('/', (req, res)=>{
+    const con = "This is the best content on the internet so far so use it wisely"
+    const params = {'title': 'PubG is the best game', "content": con}
+    res.status(200).render('index.pug', params);
+})
 
-// Our pug demo endpoint
-app.get("/demo", (req, res)=>{ 
-    res.status(200).render('demo', { title: 'Hey Pr', message: 'Hello there and thanks for telling me how to use pubG!' })
-});
+// app.post('/', (req, res)=>{
+//     console.log(req.body)
+//     const params ={'message':'Your Form has been submitted Successfully..'}
+// })
+app.post('/', (req, res)=>{
+            name = req.body.name
+            age = req.body.age
+            gender = req.body.gender
+            address = req.body.address
+            more = req.body.more
 
-app.get("/", (req, res)=>{ 
-    res.send("This is homepage of my first express app with Prashil");
-});
+            let outputToWrite = `the name of the client is ${name}, ${age} years old, ${gender}, residing at ${address}. More about him/her: ${more}`
+            fs.writeFileSync('output.txt', outputToWrite)
+            const params = {'message': 'Your form has been submitted successfully'}
+            res.status(200).render('index.pug', params);
+})
 
-app.get("/home", (req, res)=>{
-    res.send("This is home page of my first express app with Harry");
-});
-app.get("/about", (req, res)=>{
-    res.send("This is about page of my first express app with Harry");
-});
-
-
-
-app.listen(port, ()=>{
+//Start the Servers
+app.listen(port, () => {
     console.log(`The application started successfully on port ${port}`);
-});
+})
