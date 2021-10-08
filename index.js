b59a31dc82f3ad3e90ca9f47f07b7cffdfdf4662
Prashil Ramteke <87717960+prashil03081999@users.noreply.github.com>
2021-10-08 13:53:40 +0530

    Project 5

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-console.log("pr is a good boy");
+function updateMap() {
+    fetch("/data.json")
+        .then(Response => Response.json())
+        .then(rsp => { 
+            // console.log(rsp.data)
+            rsp.data.forEach(element => {
+                latitude = element.latitude
+                longitude = element.longitude
+
+
+                cases=element.infected;
+                if (cases>255){
+                    color="rgb(255,0,0)";
+                }
+                else{
+                    color=`rgb(${cases}, 0, 0)`;
+                }
+
+                // Recover ppl
+
+                // recov=element.recovered;
+                // if(recov>300){
+                //     color="rgb(0,100,0)";
+                // }
+                // else{
+                //     color=`rgb(0,${recov},0)`;
+                // }
+                // Mark on the map
+                new mapboxgl.Marker({
+                    draggable: false,
+                    color: color
+                })
+
+
+                    .setLngLat([longitude, latitude])
+                    .addTo(map);
+            });
+        })
+}
+updateMap();
