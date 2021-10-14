2d02cac27441d313d91e9c0709d537a95823ff3b
Prashil Ramteke <87717960+prashil03081999@users.noreply.github.com>
2021-10-14 10:50:12 +0530

    Project 9

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-var audio = document.createElement('audio');
-audio.setAttribute('src', 'sound.mp3'); 
-audio.loop=true;
-// audio.play();
+let screen = document.getElementById('screen');
+buttons = document.querySelectorAll('button');
+let screenValue = '';
+for (item of buttons) {
+    item.addEventListener('click', (e) => {
+        buttonText = e.target.innerText;
+        console.log('Button text is ', buttonText);
+        if (buttonText == 'X') {
+            buttonText = '*';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else if (buttonText == 'AC') {
+            screenValue = "";
+            screen.value = screenValue;
+        }
+        else if (buttonText == '=') {
+            screen.value = eval(screenValue);
+        }
+        else {
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+
+    })
+}
+
